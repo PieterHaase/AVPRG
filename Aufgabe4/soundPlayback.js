708f2a93dcc001da81db1e0f080e935956ecdc83
Jakob Sudau <jakobsudau@Jakobs-MBP.fritz.box>
2018-10-02 20:21:19 +0200

    Lösungen für Session 1

--- Aufgabe4/soundPlayback.js
+++ Aufgabe4/soundPlayback.js
@@ -1,30 +1,20 @@
-var context = new AudioContext();
-var drumpads = document.getElementsByClassName("drumpad");
-var source1, source2, source3, source4;
-var sourceBuffers = [source1, source2, source3, source4];
+var context = new AudioContext(),
+    drumpads = document.getElementsByClassName("drumpad"),
+    soundBuffers = [];
 
 for (let i = 0; i < drumpads.length; i++) {
-    getData(i);
+    soundBuffers[i] = new Audio("sounds/sound" + (i+1) + ".wav");
+    var soundNode = context.createMediaElementSource(soundBuffers[i]);
+    var gainNode = context.createGain();
+
+    gainNode.gain.value = 0.8;
+
+    soundNode.connect(gainNode);
+    gainNode.connect(context.destination);
+
     drumpads[i].addEventListener("mousedown", function (e) {playSound(i)});
 }
 
-function getData(i) {
-    var request = new XMLHttpRequest();
-    request.open('GET',  "sounds/sound" + (i + 1) + ".wav", true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        var undecodedAudio = request.response;
-
-        context.decodeAudioData(undecodedAudio, function (buffer) {
-            sourceBuffers[i] = context.createBufferSource();
-            sourceBuffers[i].buffer = buffer;
-            sourceBuffers[i].connect(context.destination);
-        });
-    };
-    request.send();
-}
-
 function playSound(i) {
-    getData(i);
-    sourceBuffers[i].start(0);
+    soundBuffers[i].play();
 }
